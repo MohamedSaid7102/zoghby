9eb5a20032479affed20402aa268d706555e51ee
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2023-02-12 21:43:50 +0200

    Initial Commit

---
+++ src/partials/Footer.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 flex felx-row flex-wrap justify-center">
+
+        {/* Top area: Blocks */}
+        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+
+          {/* 1st block */}
+          <div className="sm:col-span-12 lg:col-span-3">
+            <div className="mb-2">
+              {/* Logo */}
+              <Link to="/" className="inline-block" aria-label="Cruip">
+                <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
+                      <stop stopColor="#4FD1C5" offset="0%" />
+                      <stop stopColor="#81E6D9" offset="25.871%" />
+                      <stop stopColor="#338CF5" offset="100%" />
+                    </radialGradient>
+                  </defs>
+                  <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
+                </svg>
+              </Link>
+            </div>
+            <div className="text-sm text-gray-600">
+              <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</Link> · <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</Link>
+            </div>
+          </div>
+          {/* 2nd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Supervesion</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Dr. Sarah</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Eng. Sahar</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 3ed block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Collaborators</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Ahmed Samir EL-Zoghby</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Mohamed Ayman</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Mohamed Basha</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Mohamed ibrahim</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Abd EL-Rahman khaled</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">May Talaat</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Yara Bayoumi</Link>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
